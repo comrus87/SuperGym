d6fce94dee308462b04bfff69b63a5ef0ac1ffd1
Andrew <nk-andrew1987@mail.ru>
2020-01-15 13:38:57 +0300

    Пилим слайдер

--- source/js/script.js
+++ source/js/script.js
@@ -1,86 +1,94 @@
 'use strict';
-// var tabs = document.querySelectorAll('.subscriptions__item-month');
-// var tabControls = document.querySelector('.subscriptions__controls-list');
-// var tabContent = document.querySelectorAll('.subscriptions__tabs-list');
-// var subscriptionsPage = document.querySelector('.subscriptions');
-
-
-// function switchTabs() {
-//   function hideTabContent(a) {
-//     for (var i = a; i < tabContent.length; i++) {
-//       tabContent[i].classList.add('hide');
-//       tabContent[i].classList.remove('show');
-//       tabs[i].classList.remove('subscriptions__item-month--active');
-//     }
-//   }
-
-//   hideTabContent(1);
-
-//   function showTabContent(b, target) {
-//     if (tabContent[b].classList.contains('hide')) {
-//       tabContent[b].classList.remove('hide');
-//       tabContent[b].classList.add('show');
-//       target.classList.add('subscriptions__item-month--active');
-//     }
-//   }
-
-//   tabControls.addEventListener('click', function (evt) {
-//     var target = evt.target;
-//     if (target && target.classList.contains('subscriptions__item-month')) {
-//       for (var i = 0; i < tabs.length; i++) {
-//         if (target === tabs[i]) {
-//           hideTabContent(0);
-//           showTabContent(i, target);
-//           break;
-//         }
-//       }
-//     }
-//   });
-// }
-
-// if (subscriptionsPage) {
-//   switchTabs();
-// }
-
+var tabs = document.querySelectorAll('.subscriptions__item-month');
+var tabControls = document.querySelector('.subscriptions__controls-list');
+var tabContent = document.querySelectorAll('.subscriptions__tabs-list');
+var subscriptionsPage = document.querySelector('.subscriptions');
 
 var slides = document.querySelectorAll('.coaches__item');
 var prev = document.querySelector('.coaches__arrow-left');
 var next = document.querySelector('.coaches__arrow-right');
 
-console.log(slides.length)
 
+function switchTabs() {
+  function hideTabContent(a) {
+    for (var i = a; i < tabContent.length; i++) {
+      tabContent[i].classList.add('hide');
+      tabContent[i].classList.remove('show');
+      tabs[i].classList.remove('subscriptions__item-month--active');
+    }
+  }
+
+  hideTabContent(1);
+
+  function showTabContent(b, target) {
+    if (tabContent[b].classList.contains('hide')) {
+      tabContent[b].classList.remove('hide');
+      tabContent[b].classList.add('show');
+      target.classList.add('subscriptions__item-month--active');
+    }
+  }
+
+  tabControls.addEventListener('click', function (evt) {
+    var target = evt.target;
+    if (target && target.classList.contains('subscriptions__item-month')) {
+      for (var i = 0; i < tabs.length; i++) {
+        if (target === tabs[i]) {
+          hideTabContent(0);
+          showTabContent(i, target);
+          break;
+        }
+      }
+    }
+  });
+}
+
+if (subscriptionsPage) {
+  switchTabs();
+}
+
+
+function switchSlider(step) {
   var startIndex = 0;
-  var endIndex = 4;
+  var endIndex = step;
 
-  function showSlide (n, k) {
-    console.log(startIndex, endIndex);
+  function showSlide(n, k) {
 
-    slides.forEach(item => item.style.display = 'none');
+    slides.forEach(function (item) {
+      item.style.display = 'none';
+    });
 
 
-    for (var i = n; i < k ; i++) {
+    for (var i = n; i < k; i++) {
       slides[i].style.display = 'block';
     }
-
-  };
+  }
 
   showSlide(startIndex, endIndex);
 
-  function plusSlide (n) {
+  function plusSlide(n) {
     showSlide(startIndex += n, endIndex += n);
   }
 
 
-    next.addEventListener('click', function () {
-      if (endIndex < slides.length) {
-          plusSlide(4);
-      }
-    });
+  next.addEventListener('click', function () {
+    if (endIndex < slides.length) {
+      plusSlide(step);
+    }
+  });
 
 
-    prev.addEventListener('click', function () {
-      if (startIndex >= 4) {
-        plusSlide(-4);
-      }
-    });
+  prev.addEventListener('click', function () {
+    if (startIndex >= step) {
+      plusSlide(-step);
+    }
+  });
+}
+
+if (window.matchMedia('(max-width: 767px)').matches) {
+  switchSlider(1);
+} else if (window.matchMedia('(max-width: 1199px)').matches) {
+  switchSlider(2);
+} else {
+  switchSlider(4);
+}
 
